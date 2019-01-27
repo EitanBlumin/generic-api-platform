97444f4c8a105292f7e235c98233408407e8a93a
eitan.blumin <eitan@madeiradata.com>
2019-01-27 16:22:41 +0200

    updated api_SetFieldValue to use merge instead of update

--- generic-api-platform/GenericAPIPlatformDB/Stored Procedures/api_SetFieldValue.sql
+++ generic-api-platform/GenericAPIPlatformDB/Stored Procedures/api_SetFieldValue.sql
@@ -1,9 +1,23 @@
 ﻿CREATE PROCEDURE dbo.api_SetFieldValue(@api_target_id int, @field_name nvarchar(1000), @field_value nvarchar(4000))
 AS
 	SET NOCOUNT ON;
-
-	UPDATE dbo.api_TargetFieldValues
-		SET FieldValue = @field_value
-	WHERE
-		TargetID = @api_target_id
+	
+	WITH Trgt
+	AS
+	(
+	SELECT *
+	FROM dbo.api_TargetFieldValues
+	WHERE TargetID = @api_target_id
 	AND FieldName = @field_name
+	)
+	MERGE INTO Trgt
+	USING (VALUES(@api_target_id, @field_name, @field_value)) AS Src(TargetID, FieldName, FieldValue)
+	ON
+		Trgt.TargetID = Src.TargetID
+	AND Trgt.FieldName = Src.FieldName
+	WHEN MATCHED THEN
+		UPDATE SET FieldValue = Src.FieldValue
+	WHEN NOT MATCHED BY TARGET THEN
+		INSERT (TargetID, FieldName, FieldValue)
+		VALUES (TargetID, FieldName, FieldValue)
+	;
