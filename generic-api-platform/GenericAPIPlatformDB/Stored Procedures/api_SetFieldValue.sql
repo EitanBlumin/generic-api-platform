f61c1b7f7f905503d2371469cb0d7e18fecbe35f
eitan.blumin <eitan@madeiradata.com>
2019-01-27 16:18:39 +0200

    added api_SetFieldValue

---
+++ generic-api-platform/GenericAPIPlatformDB/Stored Procedures/api_SetFieldValue.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE dbo.api_SetFieldValue(@api_target_id int, @field_name nvarchar(1000), @field_value nvarchar(4000))
+AS
+	SET NOCOUNT ON;
+
+	UPDATE dbo.api_TargetFieldValues
+		SET FieldValue = @field_value
+	WHERE
+		TargetID = @api_target_id
+	AND FieldName = @field_name
