ce643e0082840e24d59d130da34d22b5338d74c6
eitan.blumin <eitan@madeiradata.com>
2019-02-03 15:29:59 +0200

    updated navigation icons

--- generic-api-platform/Serene/Serene.Web/Modules/Common/Navigation/NavigationItems.cs
+++ generic-api-platform/Serene/Serene.Web/Modules/Common/Navigation/NavigationItems.cs
@@ -1,4 +1,6 @@
 ﻿using Serenity.Navigation;
 using Administration = Serene.Administration.Pages;
 
-[assembly: NavigationLink(1000, "Dashboard", url: "~/", permission: "", icon: "fa-tachometer")]
+//[assembly: NavigationLink(1000, "Dashboard", url: "~/", permission: "", icon: "fa-tachometer")]
+
+[assembly: NavigationMenu(1000, "Generic API", icon: "fa-anchor")]
--- generic-api-platform/Serene/Serene.Web/Modules/GenericAPIPlatform/GenericAPIPlatformNavigation.cs
+++ generic-api-platform/Serene/Serene.Web/Modules/GenericAPIPlatform/GenericAPIPlatformNavigation.cs
@@ -1,8 +1,8 @@
 ﻿using Serenity.Navigation;
 using MyPages = Serene.GenericAPIPlatform.Pages;
 
-[assembly: NavigationLink(int.MaxValue, "GenericAPIPlatform/Api Fields", typeof(MyPages.ApiFieldsController), icon: null)]
-[assembly: NavigationLink(int.MaxValue, "GenericAPIPlatform/Api Headers", typeof(MyPages.ApiHeadersController), icon: null)]
-[assembly: NavigationLink(int.MaxValue, "GenericAPIPlatform/Api Target", typeof(MyPages.ApiTargetController), icon: null)]
-[assembly: NavigationLink(int.MaxValue, "GenericAPIPlatform/Api Target Field Values", typeof(MyPages.ApiTargetFieldValuesController), icon: null)]
-[assembly: NavigationLink(int.MaxValue, "GenericAPIPlatform/Api Type", typeof(MyPages.ApiTypeController), icon: null)]
+[assembly: NavigationLink(int.MaxValue, "Generic API/API Types", typeof(MyPages.ApiTypeController), icon: "fa-globe")]
+[assembly: NavigationLink(int.MaxValue, "Generic API/API Headers", typeof(MyPages.ApiHeadersController), icon: "fa-header")]
+[assembly: NavigationLink(int.MaxValue, "Generic API/API Fields", typeof(MyPages.ApiFieldsController), icon: "fa-file-text")]
+[assembly: NavigationLink(int.MaxValue, "Generic API/API Targets", typeof(MyPages.ApiTargetController), icon: "fa-bullseye")]
+[assembly: NavigationLink(int.MaxValue, "Generic API/API Target Field Values", typeof(MyPages.ApiTargetFieldValuesController), icon: "fa-sign-in")]
